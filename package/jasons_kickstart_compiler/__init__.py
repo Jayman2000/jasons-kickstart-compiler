688d10f45997c656f86db6c0b155646d33253124
Jason Yundt <jason@jasonyundt.email>
2022-11-07 06:46:13 -0500

    Make self_extracting_post_script() easier to use
    Before this change, self_extracting_post_script() had two arguments. The
    second one was pretty pointless. I only included that second argument
    because I couldn’t figure out how to allow that function access to
    global variables while the Jinja template was bing rendered. Now that
    I’ve figured out how to do that, I’ve removed the second argument.
    
    Additionally, this change makes the code feel much more organized.

--- package/jasons_kickstart_compiler/__init__.py
+++ package/jasons_kickstart_compiler/__init__.py
@@ -1,107 +1,10 @@
 from argparse import ArgumentParser, Namespace, RawDescriptionHelpFormatter
-from base64 import standard_b64encode
-from crypt import crypt
-from getpass import getpass
-from io import BytesIO
 from pathlib import Path
-from shlex import quote as shell_quote
-from sys import stderr
-from tarfile import TarInfo, open as open_tar
-from typing import Optional
 
 from jinja2 import Environment, FileSystemLoader, Template
 from typing_extensions import Final
 
-
-ENTRY_POINT_MISSING : Final[str] = \
-        "ERROR: A self extracting post script lacks a file named " \
-        + "entry-point. The self extracting post script won’t " \
-        + "do anything."
-OWNER_EXECUTABLE_BIT : int = 0o0100
-
-
-def get_field(
-        name : str,
-        prompt : Optional[str] = None,
-        is_password : bool = False,
-        encrypt_password : bool = True
-) -> str:
-    while True:
-        if prompt is None:
-            prompt = f"{name}: "
-        if is_password:
-            password : str = getpass(prompt)
-            if password == getpass("Type that same password again: "):
-                if encrypt_password:
-                    password = crypt(password)
-                return password
-            else:
-                print("Passwords don’t match. Try again.")
-        else:
-            return input(prompt)
-
-
-def self_extracting_post_script(
-        path : Path,
-        input_file_parent : Path
-) -> str:
-    if not path.is_absolute():
-        path = Path(input_file_parent, path)
-    # It’s kind of weird to declare a function inside of another
-    # function, but this is an easy way to give filter() access to
-    # encountered_entry_point. I could make encountered_entry_point a
-    # global variable, but it would still have to get reset every time
-    # self_extracting_post_script() gets run. Plus, keeping it a local
-    # variable theoretically allows you to run
-    # to self_extracting_post_script() multiple times at once.
-    encountered_entry_point : bool = False
-    def filter(tar_info : TarInfo) -> TarInfo:
-        if tar_info.name == "entry-point":
-            nonlocal encountered_entry_point
-            encountered_entry_point = True
-            if tar_info.mode & OWNER_EXECUTABLE_BIT == 0:
-                print(
-                        "A self-extracting post script’s entry-point’s "
-                        + "owner’s executable bit wasn’t set. "
-                        + "Automatically setting it…"
-                )
-                tar_info.mode |= OWNER_EXECUTABLE_BIT
-        return tar_info
-
-    # Credit goes to decaf (https://stackoverflow.com/users/1159217/decaf)
-    # for this idea: <https://stackoverflow.com/a/15858237/7593853>
-    tar_data = BytesIO()
-    with open_tar(fileobj=tar_data, mode='w') as tar:
-        tar.add(
-                path,
-                arcname=Path("/", path.name),
-                recursive=True,
-                filter=filter
-        )
-    if encountered_entry_point:
-        base_64_tar_data : str = standard_b64encode(tar_data.getvalue()).decode()
-        if "%end" in base_64_tar_data:
-            print(
-                    "ERROR: encoded data contains “%end”. This should "
-                    + "never happen. The resulting kickstart file won’t "
-                    + "work.",
-                    file=stderr
-            )
-        return f"""%post --interpreter=/usr/bin/bash --log=/root/jasons-kickstart-compiler_self-extracting-post.log
-declare -r temporary_directory="$(mktemp -d)"
-cd "$temporary_directory"
-echo -n {shell_quote(base_64_tar_data)} | base64 -d | tar -x
-./entry-point
-cd /
-rm -rf "$temporary_directory"
-%end
-"""
-    else:
-        ERROR_MESSAGE : Final[str] = "ERROR: didn’t find a file name " \
-                + f"“entry-point” in {str(path)}. Can’t generate a " \
-                + "self-extracting post script."
-        print(ERROR_MESSAGE, file=stderr)
-        return f"# {ERROR_MESSAGE}"
+from . import jinja_globals
 
 
 def main() -> None:
@@ -126,15 +29,14 @@ def main() -> None:
     )
     ARGUMENTS : Final[Namespace] = ARGUMENT_PARSER.parse_args()
     INPUT_FILE_PATH : Final[Path] = ARGUMENTS.file
+    jinja_globals.input_file_parent = INPUT_FILE_PATH.parent
 
     JENV : Final[Environment] = Environment(
             loader=FileSystemLoader(INPUT_FILE_PATH.parent)
     )
     JENV.globals = {
-            'get_field' : get_field,
-            'input_file_parent' : INPUT_FILE_PATH.parent,
-            'Path' : Path,
-            'self_extracting_post_script' : self_extracting_post_script
+            'jasons_kickstart_compiler' : jinja_globals,
+            'Path' : Path
     }
     TEMPLATE : Final[Template] = JENV.get_template(INPUT_FILE_PATH.name)
     OUTPUT_PATH : Final[Path] = Path("ks.cfg")
